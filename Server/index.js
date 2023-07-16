d89329b6cb1425e3d3f7ee18ccc9651dcfda9f08
paras verma <parasverma2531@gmail.com>
2023-07-17 04:00:41 +0530

    middlewares added

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 require("dotenv").config({ path: "./config.env" }); //set the path to config.env location
 const connectDb = require("./dbConnect");
 const authRouter = require("./Routers/authRouter");
+const morgan = require("morgan");
 const App = express();
+
+//middlewares:
+App.use(express.json()); // used to parse body
+App.use(morgan("common")); // morgan middleware used to create logs
+
 //forward the request to the authRouter if the endpoint is auth:
 App.use("/auth", authRouter);
 App.get("/", (req, res) => {
