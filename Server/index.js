e96ce04fdac60c1a2242d1d820baf11a8d05de11
paras verma <parasverma2531@gmail.com>
2023-07-17 18:51:17 +0530

    Add requireUser middleware

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 require("dotenv").config({ path: "./config.env" }); //set the path to config.env location
 const connectDb = require("./dbConnect");
 const authRouter = require("./Routers/authRouter");
+const postRouter = require("./Routers/postRouter");
 const morgan = require("morgan");
 const App = express();
 
-//middlewares:
+//middlewares::
 App.use(express.json()); // used to parse body
 App.use(morgan("common")); // morgan middleware used to create logs
-
-//forward the request to the authRouter if the endpoint is auth:
-App.use("/auth", authRouter);
+//Routers::
+App.use("/auth", authRouter); //forward the request to the authRouter if the endpoint is auth:
+App.use("/post", postRouter);
 App.get("/", (req, res) => {
   res.status(200).send();
 });
