4b45ff312db974ce9e7fc51010e0874ad75e56ca
paras verma <parasverma2531@gmail.com>
2023-07-18 14:01:06 +0530

    cookie parser dependency installed

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 require("dotenv").config({ path: "./config.env" }); //set the path to config.env location
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const connectDb = require("./dbConnect");
 const authRouter = require("./Routers/authRouter");
 const postRouter = require("./Routers/postRouter");
-const morgan = require("morgan");
 const App = express();
 
 //middlewares::
 App.use(express.json()); // used to parse body
 App.use(morgan("common")); // morgan middleware used to create logs
+App.use(cookieParser()); // used to parse data inside cookies
 //Routers::
 App.use("/auth", authRouter); //forward the request to the authRouter if the endpoint is auth:
 App.use("/post", postRouter);
