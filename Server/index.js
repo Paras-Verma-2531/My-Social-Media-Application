decd8db34a3fc057205760b22dd1d8658ae121fa
paras verma <parasverma2531@gmail.com>
2023-07-19 13:40:38 +0530

    CORS dependency included in backend :: use of axios to fetch login API

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config({ path: "./config.env" }); //set the path to config.env location
 const morgan = require("morgan");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDb = require("./dbConnect");
 const authRouter = require("./Routers/authRouter");
@@ -11,6 +12,12 @@ const App = express();
 App.use(express.json()); // used to parse body
 App.use(morgan("common")); // morgan middleware used to create logs
 App.use(cookieParser()); // used to parse data inside cookies
+App.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000", // request from this origin can now access our backend
+  })
+); // To include Access control policy such that our backend allows resources to accessed from other loc.
 //Routers::
 App.use("/auth", authRouter); //forward the request to the authRouter if the endpoint is auth:
 App.use("/post", postRouter);
--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Login.scss';
+import {axiosClient} from '../../Utils/axiosClient'
+import "./Login.scss";
 function Login() {
+  //fetch the data::
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+ //function to handle Data on submit
+  async function handleSubmit(event)
+ {
+   event.preventDefault();//prevent the default behaviour of form
+   const result= await axiosClient.post('/auth/login',
+   //send the data in the body of API
+   {
+     email:userEmail,
+     password:userPassword
+   })
+ }
   return (
     <div className="Login">
       <div className="login-box">
-          <h2 className="login-heading">Login</h2>
-          <form>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" className="login-input"/>
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" className="login-input"/>
-              <input type="submit" className="login-submit"/>
-          </form>
-          <p className="signup-Navigate">Create an Account?<span><Link className="link"to='/signup'>Signup</Link></span></p>
+        <h2 className="login-heading">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="login-input"
+            onChange={(event) => setUserEmail(event.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input 
+          type="password" 
+          id="password" 
+          className="login-input"
+          onChange={(event) => setUserPassword(event.target.value)}
+          />
+          <input type="submit" className="login-submit" />
+        </form>
+        <p className="signup-Navigate">
+          Create an Account?
+          <span>
+            <Link className="link" to="/signup">
+              Signup
+            </Link>
+          </span>
+        </p>
       </div>
     </div>
   );
