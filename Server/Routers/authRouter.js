fd16ee044cad9d3312f1563bfab1e3a9f0dd5815
paras verma <parasverma2531@gmail.com>
2023-07-19 08:30:08 +0530

    Refresh API modified to get request

--- Server/Routers/authRouter.js
+++ Server/Routers/authRouter.js
@@ -6,5 +6,5 @@ const {
 } = require("../Controllers/authController");
 Router.post("/login", loginController); // if endpoint is login go to the loginController
 Router.post("/signup", signupController);
-Router.post("/refresh", refreshAccessTokenController);
+Router.get("/refresh", refreshAccessTokenController);
 module.exports = Router;
