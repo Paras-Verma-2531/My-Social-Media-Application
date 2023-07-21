fc46efd8496eb5d7c0cddb68b43ca56addbb8bdd
paras verma <parasverma2531@gmail.com>
2023-07-21 23:33:46 +0530

    post schema added

---
+++ Server/Models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId, //fetch the object id from db
+    ref: "user",
+    required: true,
+  },
+  image: {
+    publicId: String,
+    url: String,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  //likes of type array:: to store details of user who like the post
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId, //fetch the object id from db
+      ref: "user", // used to make relations in mongodb
+    },
+  ],
+});
+module.exports = mongoose.model("post", postSchema);
