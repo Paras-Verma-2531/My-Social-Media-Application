db69ce46439da23742cd8567a63427b9b95a188b
paras verma <parasverma2531@gmail.com>
2023-07-21 12:39:54 +0530

    protected route for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
+import RequireUser from "./Components/RequireUser";
 import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Routes>
         {/* set path to various elements based on endpoint */}
-        <Route path="/" element={<Home/>} />
+        {/* protected route:: if user is loged-in navigate to home*/}
+        <Route element={<RequireUser />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- client/src/Components/RequireUser.js
+++ client/src/Components/RequireUser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { KEY_ACCESS_TOKEN, getItem } from "../../Utils/localStorageManager";
+import { KEY_ACCESS_TOKEN, getItem } from "../Utils/localStorageManager";
 function RequireUser() {
   //if accesstoken present :: navigate to home else login
   const accessToken = getItem(KEY_ACCESS_TOKEN);
