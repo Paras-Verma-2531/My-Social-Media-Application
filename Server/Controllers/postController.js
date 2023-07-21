b45756f0efb7c3309c8294d4fb1f9bf54f68ba3f
paras verma <parasverma2531@gmail.com>
2023-07-21 23:32:34 +0530

    createPostController added

--- Server/Controllers/postController.js
+++ Server/Controllers/postController.js
@@ -1,5 +1,23 @@
-const {success}=require('../Utils/responseWrapper');
+const Post = require("../Models/Post");
+const User = require("../Models/User");
+const { success, error } = require("../Utils/responseWrapper");
 const getAllPostController = async (req, res) => {
-  return res.send(success(200,"these are all your post"));
+  return res.send(success(200, "these are all your post"));
 };
-module.exports = getAllPostController;
+const createPostController = async (req, res) => {
+  const { caption } = req.body;
+  const owner = req._id;
+  const user = await User.findById(owner); //find the user to whom this post belong
+  try {
+    const post = await Post.create({
+      owner,
+      caption,
+    });
+    user.posts.push(post._id); //push the post id into the posts array of User schema
+    await user.save(); // to update changes to user schema
+    return res.send(success(201, post));
+  } catch (err) {
+    return res.send(error(500, err.message));
+  }
+};
+module.exports = { createPostController, getAllPostController };
