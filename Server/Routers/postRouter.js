2d8f93b0aee10b88355e6860a5670dcd280985b8
paras verma <parasverma2531@gmail.com>
2023-07-22 00:23:18 +0530

    create post controller added

--- Server/Routers/postRouter.js
+++ Server/Routers/postRouter.js
@@ -1,5 +1,9 @@
-const getAllPostController = require("../Controllers/postController");
+const {
+  getAllPostController,
+  createPostController,
+} = require("../Controllers/postController");
 const requireUserMiddleware = require("../Middlewares/requireUser");
 const postRouter = require("express").Router();
 postRouter.get("/all", requireUserMiddleware, getAllPostController);
+postRouter.post("/", requireUserMiddleware, createPostController);
 module.exports = postRouter;
