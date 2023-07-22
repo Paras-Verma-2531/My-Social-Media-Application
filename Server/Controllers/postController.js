5234104218b90fee5e8a31214562ee4a123cc1c7
paras verma <parasverma2531@gmail.com>
2023-07-22 14:47:20 +0530

    LikeAndDislikeController added

--- Server/Controllers/postController.js
+++ Server/Controllers/postController.js
@@ -4,6 +4,7 @@ const { success, error } = require("../Utils/responseWrapper");
 const getAllPostController = async (req, res) => {
   return res.send(success(200, "these are all your post"));
 };
+//controller to create new post
 const createPostController = async (req, res) => {
   const { caption } = req.body;
   const owner = req._id;
@@ -20,4 +21,31 @@ const createPostController = async (req, res) => {
     return res.send(error(500, err.message));
   }
 };
-module.exports = { createPostController, getAllPostController };
+// controller to like and dislike
+const likeAndDislikeController = async (req, res) => {
+  const { postId } = req.body;
+  const currUserId = req._id; // viewer of the post
+
+  try {
+    const post = await Post.findById(postId);
+    if (!post) return res.send(error(404, "Post not found"));
+    // if user present :: means user already liked the post
+    if (post.likes.includes(currUserId)) {
+      const index = post.likes.indxOf(currUserId);
+      post.likes.splice(index, 1); //delete 1 element present at index:
+      await post.save();
+      return res.send(success(200, "post unliked"));
+    }
+    //like the post
+    post.likes.push(currUserId); //add curr user id
+    await post.save();
+    return res.send(success(200, "post liked"));
+  } catch (err) {
+    return res.send(error(500, err.message));
+  }
+};
+module.exports = {
+  createPostController,
+  getAllPostController,
+  likeAndDislikeController,
+};
--- Server/Routers/postRouter.js
+++ Server/Routers/postRouter.js
@@ -1,9 +1,11 @@
 const {
   getAllPostController,
   createPostController,
+  likeAndDislikeController,
 } = require("../Controllers/postController");
 const requireUserMiddleware = require("../Middlewares/requireUser");
 const postRouter = require("express").Router();
 postRouter.get("/all", requireUserMiddleware, getAllPostController);
 postRouter.post("/", requireUserMiddleware, createPostController);
+postRouter.post("/like", requireUserMiddleware, likeAndDislikeController);
 module.exports = postRouter;
