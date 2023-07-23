8440024dd547bd68fdd1afeede3bdfa14a24edf7
paras verma <parasverma2531@gmail.com>
2023-07-23 14:21:34 +0530

    postRouter update

--- Server/Routers/postRouter.js
+++ Server/Routers/postRouter.js
@@ -2,10 +2,12 @@ const {
   getAllPostController,
   createPostController,
   likeAndDislikeController,
+  updatePostController,
 } = require("../Controllers/postController");
 const requireUserMiddleware = require("../Middlewares/requireUser");
 const postRouter = require("express").Router();
 postRouter.get("/all", requireUserMiddleware, getAllPostController);
 postRouter.post("/", requireUserMiddleware, createPostController);
 postRouter.post("/like", requireUserMiddleware, likeAndDislikeController);
+postRouter.post("/update", requireUserMiddleware, updatePostController);
 module.exports = postRouter;
