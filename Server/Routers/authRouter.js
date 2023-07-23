14ea3c548740ed0e96260f75293f9d81e8acb043
paras verma <parasverma2531@gmail.com>
2023-07-24 01:37:34 +0530

    logout api set

--- Server/Routers/authRouter.js
+++ Server/Routers/authRouter.js
@@ -3,8 +3,10 @@ const {
   signupController,
   loginController,
   refreshAccessTokenController,
+  logOutController,
 } = require("../Controllers/authController");
 Router.post("/login", loginController); // if endpoint is login go to the loginController
 Router.post("/signup", signupController);
 Router.get("/refresh", refreshAccessTokenController);
+Router.get("/logout", logOutController);
 module.exports = Router;
