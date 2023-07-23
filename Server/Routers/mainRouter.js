ddd99b01f44b01786cdc46863a61499de01c93f4
paras verma <parasverma2531@gmail.com>
2023-07-23 12:23:04 +0530

    MainRouter

---
+++ Server/Routers/mainRouter.js
@@ -0,0 +1,8 @@
+const authRouter = require("./authRouter");
+const postRouter = require("./postRouter");
+const userRouter = require("./userRouter");
+const mainRouter = require("express").Router();
+mainRouter.use("/auth", authRouter);
+mainRouter.use("/post", postRouter);
+mainRouter.use("/user", userRouter);
+module.exports = mainRouter;
