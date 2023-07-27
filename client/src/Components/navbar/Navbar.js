23536ee35ca9d34b856655ba03873b6d1bd95d74
paras verma <parasverma2531@gmail.com>
2023-07-27 08:51:12 +0530

    top loading bar library use

--- client/src/Components/navbar/Navbar.js
+++ client/src/Components/navbar/Navbar.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Avatar from "../avatar/Avatar";
 import { useNavigate } from "react-router-dom";
+import { AiOutlineLogout } from "react-icons/ai";
+import LoadingBar from "react-top-loading-bar";
 import "./Navbar.scss";
 function Navbar() {
+  const loadingRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  function toogleLoadingBar() {
+    if (loading) {
+      setLoading(false);
+      loadingRef.current.complete();
+    } else {
+      setLoading(true);
+      loadingRef.current.continuousStart();
+    }
+  }
   const navigate = useNavigate();
   return (
     <div className="navbar">
@@ -18,10 +31,14 @@ function Navbar() {
           >
             <Avatar />
           </div>
+          <div className="logout hover-link" onClick={toogleLoadingBar}>
+            <LoadingBar color="#4db4f8" ref={loadingRef} />
+            <AiOutlineLogout />
+          </div>
         </div>
       </div>
     </div>
   );
-}
+} 
 
 export default Navbar;
