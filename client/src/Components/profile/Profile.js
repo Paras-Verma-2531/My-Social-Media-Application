e610ff920504300e49d492035778471a833741ad
paras verma <parasverma2531@gmail.com>
2023-07-30 22:33:50 +0530

    profile pic resolved

--- client/src/Components/profile/Profile.js
+++ client/src/Components/profile/Profile.js
@@ -3,7 +3,9 @@ import "./Profile.scss";
 import userImg from "../../assets/user.png";
 import Post from "../post/Post";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Profile() {
+  const userProfile = useSelector((state) => state.appConfigReducer.myProfile);
   const navigate = useNavigate();
   return (
     <div className="profile">
@@ -16,7 +18,11 @@ function Profile() {
         </div>
         <div className="right-part">
           <div className="profile-card">
-            <img src={userImg} alt="user-img" className="user-img" />
+            <img
+              src={userProfile?.avatar?.url || userImg}
+              alt="user-img"
+              className="user-img"
+            />
             <h3 className="user-name">Paras Verma</h3>
             <div className="follower-info">
               <h4>40 followers</h4>
