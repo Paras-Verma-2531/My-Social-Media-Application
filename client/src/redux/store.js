181abc68f92f46ed35d647f7beeef754434f34fc
paras verma <parasverma2531@gmail.com>
2024-08-26 18:16:20 +0530

    store update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import appConfigReducer from "./slice/appConfig";
 import postReducer from "./slice/postsSlice";
 import feedReducer from "./slice/feedSlice";
+// The entire store has 3 major reducers namely [ appConfigReducer,postReducer,feedReducer]
 export default configureStore({
   reducer: { appConfigReducer, postReducer, feedReducer },
 });
