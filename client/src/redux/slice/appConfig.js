c6cce7bcfeeeb2e826d0cfd985d0020bd3c1e066
paras verma <parasverma2531@gmail.com>
2024-08-26 18:12:55 +0530

    appConfig/ userSlice update

--- client/src/redux/slice/appConfig.js
+++ client/src/redux/slice/appConfig.js
@@ -1,6 +1,12 @@
+/*
+  AppConfigReducer -> responsible for managing user slice, asyncronous actions
+  1: FETCH THE USER PROFILE
+  2: UPDATE USER PROFILE
+*/
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { axiosClient } from "../../Utils/axiosClient";
-// Redux thunk to call getMyProfile API asyncronously
+//NOTE: Redux thunk to call getMyProfile API asyncronously
 export const getMyProfile = createAsyncThunk(
   "user/getMyProfile",
   async (body, thunkAPI) => {
@@ -9,7 +15,7 @@ export const getMyProfile = createAsyncThunk(
       thunkAPI.dispatch(setLoading(true));
       //here result is coming from interceptor thus == actualResponse.data where actualResponse contains [config,data,..]
       const result = await axiosClient.get("/user/getMyProfile");
-      return result.response;//data is returned to extraReducers
+      return result.response;//IMP::data is returned to extraReducers
     } catch (error) {
       return Promise.reject(error);
     } finally {
@@ -17,6 +23,8 @@ export const getMyProfile = createAsyncThunk(
     }
   }
 );
+
+// THUNK TO UPDATE USER PROFILE
 export const updateProfile = createAsyncThunk(
   "user/updateProfile",
   async (body, thunkAPI) => {
@@ -31,6 +39,8 @@ export const updateProfile = createAsyncThunk(
     }
   }
 );
+
+
 //Slice which consist the states :: isLoading,myProfile
 const appConfigSlice = createSlice({
   name: "appConfigSlice",
@@ -48,7 +58,7 @@ const appConfigSlice = createSlice({
       state.toastData=action.payload;
     }
   },
-  //only used when async thunk is present
+  //IMP:only used when async thunk is present
   extraReducers: (builder) => {
     builder
       .addCase(getMyProfile.fulfilled, (state, action) => {
